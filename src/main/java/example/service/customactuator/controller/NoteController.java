a531386cee1f7b7d2684e7451415eaa4807ca88d
gregwhitaker <gwhitake@gmail.com>
2019-11-14 19:44:03 -0800

    Doing work

--- src/main/java/example/service/customactuator/controller/NoteController.java
+++ src/main/java/example/service/customactuator/controller/NoteController.java
@@ -2,6 +2,7 @@ package example.service.customactuator.controller;
 
 import example.service.customactuator.controller.model.CreateNoteRequest;
 import example.service.customactuator.data.NoteRepository;
+import example.service.customactuator.data.model.Note;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 /**
  * Controller responsible for adding and deleting notes.
@@ -24,8 +28,15 @@ public class NoteController {
     private NoteRepository repo;
 
     @PostMapping(value = "/notes")
-    public ResponseEntity<?> createNote(@RequestBody CreateNoteRequest request) {
-        return null;
+    public ResponseEntity<?> createNote(@RequestBody CreateNoteRequest request,
+                                        UriComponentsBuilder uriBuilder) {
+        Note note = repo.create(request.toNote());
+
+        URI uri = uriBuilder.path("/notes/{id}")
+                .buildAndExpand(note.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(note);
     }
 
     @GetMapping(value = "/notes")
